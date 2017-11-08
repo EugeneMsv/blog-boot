ff01e5141dfdb3bb840ba412d897fd240da55f94
Eugene <Eugene_Moiseev@epam.com>
2017-11-08 23:16:52 +0300

    update js

--- blog-service/src/main/js/app.js
+++ blog-service/src/main/js/app.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const axios = require('axios');
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import PostForm from './component/PostForm';
@@ -12,49 +12,12 @@ class App extends React.Component {
         super(props);
     }
 
-
     render() {
         return (
             <MuiThemeProvider>
                 <PostForm/>
                 {/*<PostRegisterList postRegisters={this.state.postRegisters}/>*/}
             </MuiThemeProvider>
-        )
-    }
-}
-
-class PostRegisterList extends React.Component {
-    render() {
-        const postRegisters = this.props.postRegisters.map(postRegister =>
-            <PostRegister key={postRegister.id.toString()} postRegister={postRegister}/>
-        );
-        return (
-            <table>
-                <tbody>
-                <tr>
-                    <th>Preview</th>
-                    <th>Likes</th>
-                    <th>Views</th>
-                    <th>Comments</th>
-                    <th>Created</th>
-                </tr>
-                {postRegisters}
-                </tbody>
-            </table>
-        )
-    }
-}
-
-class PostRegister extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td>{this.props.postRegister.preview}</td>
-                <td>{this.props.postRegister.likes}</td>
-                <td>{this.props.postRegister.views}</td>
-                <td>{this.props.postRegister.commentsNum}</td>
-                <td>{this.props.postRegister.createdTime}</td>
-            </tr>
         )
     }
 }
---
+++ blog-service/src/main/js/components/MainContainer.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar';
+import axios from 'axios';
+import {indigo400} from 'material-ui/styles/colors';
+
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
+
+const indigoColor = indigo400;
+
+class MainContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {postRegisters: []};
+    }
+
+    componentDidMount() {
+        axios.get('/postRegister')
+            .then(response => {
+                const page = response.data;
+                this.setState({postRegisters: page.content});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <AppBar title="Blog" style={{color: indigoColor}}/>
+                <PostRegisterTable postRegisters={this.state.postRegisters}/>
+            </div>
+
+        )
+    }
+}
+
+export default MainContainer;
+
+
+class PostRegisterTable extends React.Component {
+    render() {
+        const postRegisters = this.props.postRegisters.map(postRegister =>
+            <PostRegisterRow key={postRegister.id.toString()} postRegister={postRegister}/>
+        );
+        return (
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderColumn>Preview</TableHeaderColumn>
+                        <TableHeaderColumn>Likes</TableHeaderColumn>
+                        <TableHeaderColumn>Views</TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {postRegisters}
+                </TableBody>
+            </Table>
+        )
+    }
+}
+
+class PostRegisterRow extends React.Component {
+    render() {
+        return (
+            <TableRow>
+                <TableRowColumn>{this.props.postRegister.preview}</TableRowColumn>
+                <TableRowColumn>{this.props.postRegister.likes}</TableRowColumn>
+                <TableRowColumn>{this.props.postRegister.views}</TableRowColumn>
+            </TableRow>
+        )
+    }
+}
